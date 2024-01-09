81f1178e8b80702b4846d9103da97805157dd0fa
nataliravich <ravich.natali@gmail.com>
2024-01-09 16:59:18 +0200

    Adaptive content images for Retina displays, srcSet attribute

--- 01-stage/Retina-Lesson-3/src/App.tsx
+++ 01-stage/Retina-Lesson-3/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 import React from 'react';
 import cat from '../src/assets/images/cat-1.jpg'
+import cat2x from '../src/assets/images/cat-1@2x.jpg'
+import cat3x from '../src/assets/images/cat-1@3x.jpg'
 import cat2 from '../src/assets/images/cat-2.jpg'
 
 
@@ -9,8 +11,8 @@ function App() {
 		<div className="App">
 			<p>Hello, Retina &#128075;</p>
 
-			<div className={'bg'}></div>
-			{/*<img src={cat} alt="cat" />*/}
+			{/*<div className={'bg'}></div>*/}
+			<img src={cat} srcSet={`${cat2x} 2x, ${cat3x} 3x`} alt="cat" />
 
 		</div>
 	);
