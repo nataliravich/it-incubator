e46021bee02c15c5bb2c8ac4bb41720a81d16089
nataliravich <ravich.natali@gmail.com>
2024-01-09 16:30:25 +0200

    Adaptive images for Retina displays, background images

---
+++ 01-stage/Retina-Lesson-3/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+import React from 'react';
+import cat from '../src/assets/images/cat-1.jpg'
+import cat2 from '../src/assets/images/cat-2.jpg'
+
+
+function App() {
+	return (
+		<div className="App">
+			<p>Hello, Retina &#128075;</p>
+
+			<div className={'bg'}></div>
+			{/*<img src={cat} alt="cat" />*/}
+
+		</div>
+	);
+}
+
+export default App;
+
+
+
